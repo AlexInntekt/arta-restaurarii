5b2fa0116afa4197f4f35ac9fe620cd213d8a585
Mihai Alexandru Manolescu <alex.inntekt@gmail.com>
2019-03-28 17:15:44 +0200

    save progress

---
+++ src/static_dirs/js/script.js
@@ -0,0 +1,13 @@
+$(function() {
+    $("#home a:contains('Home')").parent().addClass('active');
+    $("#Restaurare a:contains('Restaurare')").parent().addClass('active');
+    $("#contact a:contains('Contact')").parent().addClass('active');
+
+  
+
+    
+});
+
+
+
+
