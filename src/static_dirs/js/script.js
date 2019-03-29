56d8ac0badcebf4bc68c76f5f6874c9b2eb38e4d
Mihai Alexandru Manolescu <alex.inntekt@gmail.com>
2019-03-29 13:11:01 +0200

    worked on menu dynamics

--- src/static_dirs/js/script.js
+++ src/static_dirs/js/script.js
@@ -1,13 +1,2 @@
-$(function() {
-    $("#home a:contains('Home')").parent().addClass('active');
-    $("#Restaurare a:contains('Restaurare')").parent().addClass('active');
-    $("#contact a:contains('Contact')").parent().addClass('active');
-
-  
-
-    
-});
-
-
 
 
